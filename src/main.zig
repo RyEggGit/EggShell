25e7a8d796dc6fa29a44cf5e57464c663b9cf53c
Ryan Eggens <81097013+RyEggGit@users.noreply.github.com>
2026-04-05 17:51:43 -0400

    add ability to execute any command in PATH

--- src/main.zig
+++ src/main.zig
@@ -7,10 +7,6 @@ var stdin_buffer: [4096]u8 = undefined;
 var stdin_reader = std.fs.File.stdin().readerStreaming(&stdin_buffer);
 const stdin = &stdin_reader.interface;
 
-const Command = struct {
-    command: []const u8,
-    args: [][]const u8,
-};
 
 const CommandType = enum {
     exit,
@@ -19,23 +15,18 @@ const CommandType = enum {
     unknown,
 };
 
-fn parseCommand(allocator: std.mem.Allocator) !Command {
+fn parseCommand(allocator: std.mem.Allocator) ![]const[]const u8 {
     const fullCommand = try stdin.takeDelimiter('\n');
     var it = std.mem.splitSequence(u8, fullCommand.?, " ");
 
-    const command = it.next().?;
-
-    var args_list: std.ArrayList([]const u8) = .empty;
-    defer args_list.deinit(allocator);
+    var command: std.ArrayList([]const u8) = .empty;
+    defer command.deinit(allocator);
 
     while (it.next()) |arg| {
-        try args_list.append(allocator, arg);
+        try command.append(allocator, arg);
     }
-
-    return Command{
-        .command = command,
-        .args = try args_list.toOwnedSlice(allocator),
-    };
+    
+    return command.toOwnedSlice(allocator);
 }
 
 fn commandType(cmd: []const u8) CommandType {
@@ -45,7 +36,10 @@ fn commandType(cmd: []const u8) CommandType {
     return .unknown;
 }
 
-fn checkPath(env_map: *std.process.EnvMap, allocator: std.mem.Allocator, query: []const u8) ![][]const u8 {
+// Read "PATH" env varaible and do some pre process to split the
+// files into there own strings. 
+fn readPath(allocator: std.mem.Allocator) ![][]const u8 {
+    const env_map = try allocator.create(std.process.EnvMap);
     env_map.* = try std.process.getEnvMap(allocator);
 
     const path = env_map.get("PATH") orelse "";
@@ -55,14 +49,52 @@ fn checkPath(env_map: *std.process.EnvMap, allocator: std.mem.Allocator, query: 
     defer paths.deinit(allocator);
 
     while (dirs.next()) |dir| {
-        const full_path = try std.fs.path.join(allocator, &[_][]const u8{ dir, query });
-
-        if (std.fs.accessAbsolute(full_path, .{})) {
-            try paths.append(allocator, full_path);
-        } else |_| {}
+        try paths.append(allocator, dir);
     }
 
     return paths.toOwnedSlice(allocator);
+}
+
+// given a command check if we can find a matching absolute path for
+// it inside a list of paths
+fn findMatchingPath(allocator: std.mem.Allocator, paths: [][]const u8, command: []const u8) ![][]const u8 {
+    var matches: std.ArrayList([]const u8) = .empty;
+    defer matches.deinit(allocator);
+
+    for (paths) |path| {
+        const full_path = try std.fs.path.join(allocator, &[_][]const u8{ path, command });
+
+        if (std.fs.accessAbsolute(full_path, .{})) {
+            try matches.append(allocator, full_path);
+        } else |_| {}
+    }
+    
+    return matches.toOwnedSlice(allocator);
+}
+
+
+// Execute an arbritary command in the current terminal window (ex: pwd,..) 
+fn execute(command: []const []const u8, allocator: std.mem.Allocator) !u8 {
+    var child = std.process.Child.init(command, allocator);
+
+    child.stdin_behavior  = .Inherit;
+    child.stdout_behavior = .Inherit;
+    child.stderr_behavior = .Inherit;
+
+    child.spawn() catch {
+        std.debug.print("command not found: {s}\n", .{command[0]});
+        return 127;
+    };
+
+    return switch (try child.wait()) {
+        .Exited  => |code| code,
+        .Signal  => |sig|  blk: { 
+            std.debug.print("killed by signal {d}\n", .{sig});
+            break :blk 1;
+        },
+        .Stopped => 1,
+        .Unknown => 1,
+    };
 }
 
 pub fn main() !void {
@@ -70,37 +102,47 @@ pub fn main() !void {
     defer arena.deinit();
 
     const allocator = arena.allocator();
-    const env_map = try allocator.create(std.process.EnvMap);
+    const paths = try readPath(allocator);
 
     while (true) {
         try stdout.print("$ ", .{});
         const command = try parseCommand(allocator);
 
-        switch (commandType(command.command)) {
+        switch (commandType(command[0])) {
             .exit => std.process.exit(0),
             .echo => {
-                const args = try std.mem.join(allocator, " ", command.args);
+                const args = try std.mem.join(allocator, " ", command[1..]);
                 try stdout.print("{s}\n", .{args});
             },
             .type => {
-                const arg = command.args[0];
-                if (command.args.len > 0 and commandType(arg) != .unknown) {
+                const arg = command[1];
+                if (commandType(arg) != .unknown) {
                     try stdout.print("{s} is a shell builtin\n", .{arg});
                     continue;
                 }
+                const matches = try findMatchingPath(allocator, paths, arg);
 
-                const paths = try checkPath(env_map, allocator, arg);
-
-                if (paths.len == 0) {
+                if (matches.len == 0) {
                     try stdout.print("{s}: not found\n", .{arg});
                 } else {
-                    for (paths) |path| {
-                        try stdout.print("{s} is {s}\n", .{ arg, path });
+                    for (matches) |match| {
+                        try stdout.print("{s} is {s}\n", .{ arg, match });
                     }
                 }
+
             },
             .unknown => {
-                try stdout.print("{s}: command not found\n", .{command.command});
+                const matches = try findMatchingPath(allocator, paths, command[0]);
+
+                if (matches.len == 0) {
+                    try stdout.print("{s}: not found\n", .{command[0]});
+                } else {
+                    const response = try execute(command, allocator);
+                    if (response != 0) {
+                        try stdout.print("{s}: failed with status code {d}\n", .{command[0], response});
+                    }
+                }
+
             },
         }
     }
